c7d3242f99f630d838da43b9ea5e5bbeb0905127
maksik852002 <49224285+maksik852002@users.noreply.github.com>
2020-11-05 22:09:05 +0600

    Redux (#7)
    * create store
    
    * continue create store
    
    * finish redux and table logic
    
    Co-authored-by: Maxim <maksik852002@google.com>

--- src/core/dom.js
+++ src/core/dom.js
@@ -14,9 +14,9 @@ class Dom {
   }
 
   text(text) {
-    if (typeof text === 'string') {
-    this.$el.textContent = text
-    return this
+    if (typeof text !== 'undefined') {
+      this.$el.textContent = text
+      return this
     }
     if (this.$el.tagName.toLowerCase() === 'input') {
       return this.$el.value.trim()
@@ -75,6 +75,13 @@ class Dom {
     Object.keys(styles).map(key => this.$el.style[key] = styles[key])
   }
 
+  getStyles(styles = []) {
+    return styles.reduce((res, s) => {
+      res[s] = this.$el.style[s]
+      return res
+    }, {})
+  }
+
   id(parse) {
     if (parse) {
       const parsed = this.id().split(':')
@@ -89,6 +96,14 @@ class Dom {
   focus() {
     this.$el.focus()
     return this
+  }
+
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value)
+      return this
+    }
+    return this.$el.getAttribute(name)
   }
 
   addClass(className) {
